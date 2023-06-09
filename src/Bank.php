2503ad1962c72c0714585e9ed5f4006acb7ddd52
Laurent VOULLEMIER <laurent.voullemier@gmail.com>
2023-06-09 21:21:52 +0200

    Introduce bank

---
+++ src/Bank.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\TddTraining;
+
+final class Bank
+{
+    public function addRate(string $from, string $to, float $rate): void
+    {
+    }
+}
--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -2,11 +2,21 @@
 
 namespace App\TddTraining\Tests;
 
+use App\TddTraining\Bank;
 use App\TddTraining\MoneyFactory;
 use PHPUnit\Framework\TestCase;
 
 final class MoneyTest extends TestCase
 {
+    private Bank $bank;
+
+    protected function setUp(): void
+    {
+        $this->bank = new Bank();
+        $this->bank->addRate('USD', 'CHF', 2);
+        $this->bank->addRate('CHF', 'USD', .5);
+    }
+
     public function testEquals(): void
     {
         $dollar = MoneyFactory::dollar(5);
@@ -18,7 +28,7 @@ final class MoneyTest extends TestCase
     public function testSimpleDollarAddition(): void
     {
         $dollar = MoneyFactory::dollar(6);
-        $dollar = $dollar->plus(MoneyFactory::dollar(6));
+        $dollar = $dollar->plus(MoneyFactory::dollar(6), $this->bank);
 
         $this->assertTrue($dollar->equals(MoneyFactory::dollar(12)));
     }
@@ -26,7 +36,7 @@ final class MoneyTest extends TestCase
     public function testSimpleFrancAddition(): void
     {
         $franc = MoneyFactory::franc(6);
-        $franc = $franc->plus(MoneyFactory::franc(6));
+        $franc = $franc->plus(MoneyFactory::franc(6), $this->bank);
 
         $this->assertTrue($franc->equals(MoneyFactory::franc(12)));
     }
@@ -34,11 +44,11 @@ final class MoneyTest extends TestCase
     public function testMixedAddition(): void
     {
         $dollar = MoneyFactory::dollar(6);
-        $dollar = $dollar->plus(MoneyFactory::franc(6));
+        $dollar = $dollar->plus(MoneyFactory::franc(6), $this->bank);
         $this->assertTrue($dollar->equals(MoneyFactory::dollar(9)));
 
         $franc = MoneyFactory::franc(6);
-        $franc = $franc->plus(MoneyFactory::dollar(6));
+        $franc = $franc->plus(MoneyFactory::dollar(6), $this->bank);
         $this->assertTrue($franc->equals(MoneyFactory::franc(18)));
     }
 }
